772b749f7aa2a85121cc494cbdad022f88575f2d
Dublin Anondson <stratophone2112@gmail.com>
2018-12-22 13:01:26 -0600

    Passing params from the front end is a go. Need to implement logic around guessing and storage.

--- hangman.js
+++ hangman.js
@@ -1,21 +1,27 @@
-
-
-var word = ['TEST', 'FALSE PROPHET', 'INTERLOPER']
+"use strict";
+var word = ['TEST', 'FALSE PROPHET', 'INTERLOPER'];
 var chosenWord;
-function initializeWord(){
+var guessedLetters = [];
+
+//TODO convert this to a closure maybe? 
+function initializeWord() {
     //TODO read in a dictionary, get a word out of it. 
-    chosenWord = word[Math.random(0, 2)];
-    console.log(word);
+    chosenWord = String(word[Math.floor(Math.random() * Math.floor(2))]);
+    console.log("INITIALIZING CHOSEN WORD TO: " + chosenWord);
 }
 
-function guess(guess){
-  if(String.length(guess) != 1)
-  {
-      return "You might check the length of your guess stranger..";
-  } else {
-      if(chosenWord.includes(guess))
-      {
-          
-      }
-  }
+function guess(inputGuess) {
+    let guess = String(inputGuess).toUpperCase();
+    
+    if (guess.length != 1) {
+        console.log("You're guess was blank or greater than one... D: D: D: ");
+        //TODO logic to decrement the amout of valid guesses left. 
+        return "You might check the length of your guess stranger..";
+    } else {
+        console.log("wow ya made it to the else statement, the chosen word is: ", chosenWord);
+        if (chosenWord.includes(guess)) {
+
+            console.log("Your guess of: ", guess, " was correct! Adding to the list of guessed characters.");
+        }
+    }
 }

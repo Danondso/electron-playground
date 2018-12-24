8da5d52361cfd1957c3f5dd9c019a092304ff65c
Dublin Anondson <stratophone2112@gmail.com>
2018-12-23 21:41:31 -0600

    Added a p tag that shows the current result and guesses. Added bootstrap (more to come later). Moved the script tag to the bottom of the html doc.

--- hangman.js
+++ hangman.js
@@ -2,26 +2,40 @@
 var word = ['TEST', 'FALSE PROPHET', 'INTERLOPER'];
 var chosenWord;
 var guessedLetters = [];
+var totalGuesses;
 
 //TODO convert this to a closure maybe? 
 function initializeWord() {
     //TODO read in a dictionary, get a word out of it. 
     chosenWord = String(word[Math.floor(Math.random() * Math.floor(2))]);
     console.log("INITIALIZING CHOSEN WORD TO: " + chosenWord);
+    totalGuesses = chosenWord.length;
+    document.getElementById("result").innerHTML = createAnswer(chosenWord, guessedLetters);
 }
 
 function guess(inputGuess) {
-    let guess = String(inputGuess).toUpperCase();
-    
-    if (guess.length != 1) {
-        console.log("You're guess was blank or greater than one... D: D: D: ");
-        //TODO logic to decrement the amout of valid guesses left. 
-        return "You might check the length of your guess stranger..";
-    } else {
-        console.log("wow ya made it to the else statement, the chosen word is: ", chosenWord);
-        if (chosenWord.includes(guess)) {
 
+    let guess = String(inputGuess).toUpperCase();
+
+    if (guess.length == 1) {
+        if (chosenWord.includes(guess)) {
             console.log("Your guess of: ", guess, " was correct! Adding to the list of guessed characters.");
+            guessedLetters.push(guess);
+            totalGuesses--;
+            document.getElementById("result").innerHTML = createAnswer(chosenWord, guessedLetters);
         }
     }
+}
+
+function createAnswer(chosenWord, guessedLetters) {
+    let result = "";
+
+    for (let i of Array.from(chosenWord)) {
+        if (guessedLetters.includes(i)) {
+            result += (i + " ");
+        } else {
+            result += "_ ";
+        }
+    }
+    return result;
 }

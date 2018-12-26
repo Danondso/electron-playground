875256cdaeaf3cda5fc7926dac4570c88a224893
Danondso <Stratophone2112@gmail.com>
2018-12-26 16:39:38 -0600

    Added in some really basic boostrap styling and css. It's real ugly now.

--- hangman.js
+++ hangman.js
@@ -1,15 +1,15 @@
 "use strict";
-var word = ['TEST', 'FALSE PROPHET', 'INTERLOPER']; //TODO expand the dictionary. 
+var word = ['TEST', 'FALSE PROPHET', 'INTERLOPER'];
 var chosenWord;
 var guessedLetters;
 var totalGuesses;
 
 function initializeGame() {
-    //TODO read in a dictionary, get a word out of it. 
     totalGuesses = 6;
     guessedLetters = [];
     chosenWord = String(word[Math.floor(Math.random() * Math.floor(3))]);
     document.getElementById("result").innerHTML = createWordState(chosenWord, guessedLetters);
+    document.getElementById("guessResultMessage").innerHTML = "Input a letter to start playing!";
 }
 
 function guess(inputGuess) {
@@ -27,25 +27,25 @@ function guess(inputGuess) {
                     let currentStateOfWord = createWordState(chosenWord, guessedLetters);
 
                     if (currentStateOfWord == chosenWord) {
-                        document.getElementById("remainingGuesses").innerHTML = "You guessed the word! Congratulations.";
+                        document.getElementById("guessResultMessage").innerHTML = "You guessed the word! Congratulations.";
                     } else {
-                        document.getElementById("remainingGuesses").innerHTML = "You guessed correctly!";
+                        document.getElementById("guessResultMessage").innerHTML = "You guessed correctly!";
                     }
                     document.getElementById("result").innerHTML = currentStateOfWord;
 
                 } else {
-                    document.getElementById("remainingGuesses").innerHTML = "You've already guessed " + guess + ", try again.";
+                    document.getElementById("guessResultMessage").innerHTML = "You've already guessed '" + guess + "', try again.";
                 }
 
             } else {
                 totalGuesses--;
-                document.getElementById("remainingGuesses").innerHTML = "You guessed incorrectly! Guesses remaining: " + totalGuesses;
+                document.getElementById("guessResultMessage").innerHTML = "You guessed incorrectly! Guesses remaining: " + totalGuesses;
             }
         } else {
-            document.getElementById("remainingGuesses").innerHTML = "You might want to check the length of your guesses stranger..";
+            document.getElementById("guessResultMessage").innerHTML = "You might want to check the length of your guesses stranger..";
         }
     } else {
-        document.getElementById("remainingGuesses").innerHTML = "You've run out of guesses, game over ^_^";
+        document.getElementById("guessResultMessage").innerHTML = "You've run out of guesses, game over ^_^";
     }
 }
 

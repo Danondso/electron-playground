5e0b46219ca95b2b288967aef519b24197e26d8d
Dublin Anondson <stratophone2112@gmail.com>
2018-12-25 20:18:58 -0600

    Finished the hangman logic.

--- hangman.js
+++ hangman.js
@@ -1,41 +1,64 @@
 "use strict";
-var word = ['TEST', 'FALSE PROPHET', 'INTERLOPER'];
+var word = ['TEST', 'FALSE PROPHET', 'INTERLOPER']; //TODO expand the dictionary. 
 var chosenWord;
-var guessedLetters = [];
+var guessedLetters;
 var totalGuesses;
 
-//TODO convert this to a closure maybe? 
-function initializeWord() {
+function initializeGame() {
     //TODO read in a dictionary, get a word out of it. 
-    chosenWord = String(word[Math.floor(Math.random() * Math.floor(2))]);
-    console.log("INITIALIZING CHOSEN WORD TO: " + chosenWord);
-    totalGuesses = chosenWord.length;
-    document.getElementById("result").innerHTML = createAnswer(chosenWord, guessedLetters);
+    totalGuesses = 6;
+    guessedLetters = [];
+    chosenWord = String(word[Math.floor(Math.random() * Math.floor(3))]);
+    document.getElementById("result").innerHTML = createWordState(chosenWord, guessedLetters);
 }
 
 function guess(inputGuess) {
 
     let guess = String(inputGuess).toUpperCase();
 
-    if (guess.length == 1) {
-        if (chosenWord.includes(guess)) {
-            console.log("Your guess of: ", guess, " was correct! Adding to the list of guessed characters.");
-            guessedLetters.push(guess);
-            totalGuesses--;
-            document.getElementById("result").innerHTML = createAnswer(chosenWord, guessedLetters);
+    if (totalGuesses != 0) {
+
+        if (guess.length == 1) {
+
+            if (chosenWord.includes(guess)) {
+
+                if (!guessedLetters.includes(guess)) {
+                    guessedLetters.push(guess);
+                    let currentStateOfWord = createWordState(chosenWord, guessedLetters);
+
+                    if (currentStateOfWord == chosenWord) {
+                        document.getElementById("remainingGuesses").innerHTML = "You guessed the word! Congratulations.";
+                    } else {
+                        document.getElementById("remainingGuesses").innerHTML = "You guessed correctly!";
+                    }
+                    document.getElementById("result").innerHTML = currentStateOfWord;
+
+                } else {
+                    document.getElementById("remainingGuesses").innerHTML = "You've already guessed " + guess + ", try again.";
+                }
+
+            } else {
+                totalGuesses--;
+                document.getElementById("remainingGuesses").innerHTML = "You guessed incorrectly! Guesses remaining: " + totalGuesses;
+            }
+        } else {
+            document.getElementById("remainingGuesses").innerHTML = "You might want to check the length of your guesses stranger..";
         }
+    } else {
+        document.getElementById("remainingGuesses").innerHTML = "You've run out of guesses, game over ^_^";
     }
 }
 
-function createAnswer(chosenWord, guessedLetters) {
+function createWordState(chosenWord, guessedLetters) {
     let result = "";
 
     for (let i of Array.from(chosenWord)) {
-        if (guessedLetters.includes(i)) {
-            result += (i + " ");
-        } else {
-            result += "_ ";
-        }
+        if (guessedLetters.includes(i))
+            result += (i);
+        else if (i == " ")
+            result += " ";
+        else
+            result += "-";
     }
     return result;
 }

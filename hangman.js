92b870ee4778030b9657a82fd35be34a231c764a
Dublin Anondson <stratophone2112@gmail.com>
2018-12-26 20:12:14 -0600

    Reworked some of the logic. Added a guessed letters section.

--- hangman.js
+++ hangman.js
@@ -20,29 +20,29 @@ function guess(inputGuess) {
 
         if (guess.length == 1) {
 
+            if (!guessedLetters.includes(guess)) {
+                guessedLetters.push(guess);
+            } else {
+                document.getElementById("guessResultMessage").innerHTML = "You've already guessed '" + guess + "', try again.";
+            }
+
             if (chosenWord.includes(guess)) {
+                let currentStateOfWord = createWordState(chosenWord, guessedLetters);
 
-                if (!guessedLetters.includes(guess)) {
-                    guessedLetters.push(guess);
-                    let currentStateOfWord = createWordState(chosenWord, guessedLetters);
-
-                    if (currentStateOfWord == chosenWord) {
-                        document.getElementById("guessResultMessage").innerHTML = "You guessed the word! Congratulations.";
-                    } else {
-                        document.getElementById("guessResultMessage").innerHTML = "You guessed correctly!";
-                    }
-                    document.getElementById("result").innerHTML = currentStateOfWord;
-
+                if (currentStateOfWord == chosenWord) {
+                    totalGuesses = 0;
+                    document.getElementById("guessResultMessage").innerHTML = "You guessed the word! Congratulations.";
                 } else {
-                    document.getElementById("guessResultMessage").innerHTML = "You've already guessed '" + guess + "', try again.";
+                    document.getElementById("guessResultMessage").innerHTML = "You guessed correctly!";
                 }
-
+                document.getElementById("result").innerHTML = currentStateOfWord;
             } else {
                 totalGuesses--;
                 document.getElementById("guessResultMessage").innerHTML = "You guessed incorrectly! Guesses remaining: " + totalGuesses;
             }
+            document.getElementById("guessedLetters").innerHTML = guessedLetters;
         } else {
-            document.getElementById("guessResultMessage").innerHTML = "You might want to check the length of your guesses stranger..";
+            document.getElementById("guessResultMessage").innerHTML = "Your guess length was greater than one.";
         }
     } else {
         document.getElementById("guessResultMessage").innerHTML = "You've run out of guesses, game over ^_^";
